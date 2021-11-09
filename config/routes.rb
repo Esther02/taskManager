8464ba8126e3b63ae64e593bcab4d174e0a8a6ba
Esther Carminatti <yunalesca002@gmail.com>
2021-11-09 13:03:12 +0000

    implemented find method and added links to index and show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Read all the tasks(index)
   get '/tasks', to: 'tasks#index'
+  # Read one task(show)
+  get '/tasks/:id', to: 'tasks#show'
+
+
 end
