7c8cc163b7b3ac23ef2c66fd814f954529183376
Esther Carminatti <yunalesca002@gmail.com>
2021-11-09 15:40:37 +0000

    all methods done

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
   get '/tasks/new', to: 'tasks#new'
   post '/tasks', to: 'tasks#create'
   # Read one task(show)
-  get '/tasks/:id', to: 'tasks#show'
+  get '/tasks/:id', to: 'tasks#show', as: "task"
   # Update a task(edit + update)
-  get '/tasks/:id/edit', to: 'tasks#edit'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: "edit_task"
   patch '/tasks/:id', to: 'tasks#update'
+  # Delete a task
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
